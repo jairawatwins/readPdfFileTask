8f5dfde40d63e776c04b9a5000696b4c545ddd27
jairawatwins <jaisinghwins@gmail.com>
2023-08-03 17:18:02 +0530

    openEncriptedPdfAndPrintPasswordInConsol

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,28 +4,70 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    public static java.util.List<LocalDate> getDateRange(LocalDate startDate, LocalDate endDate) {
+        List<LocalDate> dateRange = new ArrayList<>();
+
+        LocalDate currentDate = startDate;
+        while (!currentDate.isEqual(endDate)) {
+            dateRange.add(currentDate);
+            currentDate = currentDate.plusDays(1);
+        }
+
+        return dateRange;
+    }
+
+    public static String convertDateFormat(String startDate, String endDate) {
+        // Parse the input date using the "yyyy-MM-dd" format
+        LocalDate date = LocalDate.parse(startDate);
+        // Format the date using the desired output format
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(endDate);
+        String convertedDate = date.format(outputFormatter);
+
+        return convertedDate;
+    }
+
     public static void main(String[] args) {
+
         try {
-            String password = "16061998";
+            LocalDate startDate = LocalDate.of(1980, 1, 1);
+            LocalDate endDate = LocalDate.of(2023, 8, 3);
+
+            List<LocalDate> dateRange = getDateRange(startDate, endDate);
+            String outputFormat = "ddMMyyyy";
+
+//            String password = "16061998";
             File encryptedFile = new File("C:\\Users\\Ditsdev154\\Downloads\\TestDoc-protected.pdf");
             File decryptedFile = new File("C:\\Users\\Ditsdev154\\Downloads\\TestDoc-decrypted.pdf");
             boolean passwordFound = false;
             PDDocument document = new PDDocument();
-            //Start Loop
-            for(;;) {
+
+            // Print the dateRange list
+
+//            //Start Loop
+            for(LocalDate date : dateRange) {
+                String convertedDate = convertDateFormat(String.valueOf(date), outputFormat);
                 try {
-                    document = PDDocument.load(encryptedFile, password);
+                    document = PDDocument.load(encryptedFile, convertedDate);
                     passwordFound = true;
+//                    System.out.println(convertedDate);
+
                 } catch (Exception ex) {
                     ex.getMessage();
                 }
                 if (passwordFound) {
                     //Print Correct Password
+                    System.out.println(convertedDate);
                     break;
                 }
+
+
             }
             //EndLoop
 
@@ -45,5 +87,6 @@ public class Main {
             e.printStackTrace();
         }
     }
+
 
 }
